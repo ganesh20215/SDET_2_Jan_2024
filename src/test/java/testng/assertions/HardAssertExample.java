8deaa57dc05ab6d083e53440bcd765486a1a00ec
Ganesh Jadhav <ganesh20215@gmail.com>
2024-05-16 08:29:21 +0530

    fixed the comment

--- src/test/java/testng/assertions/HardAssertExample.java
+++ src/test/java/testng/assertions/HardAssertExample.java
@@ -20,5 +20,6 @@ public class HardAssertExample {
         Assert.assertEquals(title, "Rediffmail", "Rediffmail text should be match");
         Assert.assertTrue(userNameTextBox.isEnabled(), "userNameTextBox should be true");
 		System.out.println("Hello Everyone, Good Evening... How Are You?");
+		driver.close();
     }
 }
